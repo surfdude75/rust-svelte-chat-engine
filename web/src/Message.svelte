917d38b090c9c89463f3966ced567677c6dc96f4
Rafael Sobral <rafaelsobral@pm.me>
2024-04-24 10:17:14 -0300

    print object as json

--- web/src/Message.svelte
+++ web/src/Message.svelte
@@ -1,11 +1,12 @@
 <script>
   export let message;
+  let content = message.message;
+  if (typeof content == 'object') content = JSON.stringify(content);
 </script>
 
 <blockquote class=" mb-2">
-  <p class="text-gray-800 sm:text-xl dark:text-white">{message.message}</p>
+  <p class="text-gray-800 sm:text-xl dark:text-white">{content}</p>
   <div class="text-base font-semibold text-gray-800 dark:text-neutral-400">{ message.sender_id }</div>
   <div class="text-xs text-gray-500 dark:text-neutral-500">{new Date(message.received_at)}</div>
 </blockquote>
-
 
