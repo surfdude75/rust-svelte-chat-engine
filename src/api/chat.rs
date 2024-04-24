d223195a119009dcf3557fa894e3c0328d2cf1c5
Rafael Sobral <rafaelsobral@pm.me>
2024-04-24 10:17:53 -0300

    create room method

--- src/api/chat.rs
+++ src/api/chat.rs
@@ -1,9 +1,10 @@
 use std::sync::Arc;
 
-use futures_util::stream::SplitSink;
+use futures_util::{future::join_all, stream::SplitSink};
+use uuid::Uuid;
 use warp::filters::ws::{Message, WebSocket};
 
-use self::{client::WebSocketClient, engine::ChatEngine};
+use self::{client::WebSocketClient, engine::ChatEngine, room::WebSocketRoom};
 
 pub mod client;
 mod engine;
@@ -21,6 +22,27 @@ impl ChatManager {
     }
     pub async fn remove_client(&self, client_id: &uuid::Uuid) {
         self.engine.remove_client(client_id).await;
+    }
+
+    pub async fn create_room(&self, clients: Vec<&Uuid>) -> Option<Arc<WebSocketRoom>> {
+        let mut clients: Vec<Arc<WebSocketClient>> = join_all(
+            clients
+                .iter()
+                .map(|client_id| async { self.engine.get_client(client_id).await }),
+        )
+        .await
+        .into_iter()
+        .flatten()
+        .collect();
+
+        if let Some(creator) = clients.pop() {
+            let room = WebSocketRoom::create_room(&self.engine, creator.get_id()).await;
+            for client in clients {
+                client.join_room(room.get_id()).await;
+            }
+            return Some(room);
+        }
+        None
     }
 }
 
